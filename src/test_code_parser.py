45f1c435b0c7272e6e70abb0c97cf86ee00eab1e
Vestjevs <sag@nita.ru>
2023-10-06 10:03:15 +0300

    add simple check to find new scope

--- src/test_code_parser.py
+++ src/test_code_parser.py
@@ -1,6 +1,15 @@
 import code_parser
 
-TEST_FILENAME = 'src\check_copies.py'
+TEST_FILENAME = 'src\\test_python_script.py'
 
-def test_parse_python_script() -> list:
-    return code_parser.parse_python_script(TEST_FILENAME)
+def test_parse_py_script() -> dict:
+    return code_parser.parse_py_script(TEST_FILENAME)
+
+def test_parse_cpp_header() -> dict:
+    return code_parser.parse_cpp_header(TEST_FILENAME)
+
+def test_parse_py_function() -> list:
+    return code_parser.parse_py_function(TEST_FILENAME)
+
+def test_parse_cpp_impl() -> list:
+    pass
---
+++ src/test_python_script.py
@@ -0,0 +1,14 @@
+def foo() -> None:
+    print("Hello world!")
+    
+def bar() -> None:
+    print("Hello world!")
+    
+def foo_bar() -> None:
+    print("Hello world!")
+    
+class Foo:
+    pass
+
+class Bar:
+    pass
