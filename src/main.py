52352496ec21da4089a2fa04ad22a36039f245c5
Vestjevs <ac.lbear@yandex.ru>
2024-03-04 13:54:53 +0300

    updated gitignore

--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ pdf = FPDF()
 squad_dataset = load_dataset('squad')
 
 if __name__ == "__main__":
-     if not os.path.exists(f"{structs.DIR_OUTPUT}"): os.mkdir(f"{structs.DIR_OUTPUT}", mode=0x666)
-     
-     start = time.time()
-     test_code_parser.test_parse_cpp_func_intro()
-     end = time.time()
-     print(f"Ellapsed time(ms): {(end - start) * 10 ** 3}")
-     
+    if not os.path.exists(f"{structs.DIR_OUTPUT}"):
+        os.mkdir(f"{structs.DIR_OUTPUT}", mode=0x666)
+
+    start = time.time()
+    test_code_parser.test_parse_cpp_func_intro()
+    end = time.time()
+    print(f"Ellapsed time(ms): {(end - start) * 10 ** 3}")
